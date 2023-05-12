f0f1b73872424d0d88ebc76ed198fcfcb92cf2cf
Saveur <bikorimanaxavier@gmail.com>
2023-05-12 07:37:14 -0400

    Separated Backend and Frontend

--- backend/middlewares/pageNotFound.js
+++
@@ -1,5 +0,0 @@
-const ErrorHandler = require("../utils/ErrorHandler");
-
-module.exports =(req,res,next)=>{
-    return next(new ErrorHandler("Page Not Found",404));
-}
--- backend/utils/updateStock.js
+++
@@ -1,9 +0,0 @@
-const Product = require("../models/productModel")
-
-module.exports = async(id,quantity)=>{
-    const product = await Product.findById(id);
-
-    product.stock = product.stock - quantity;
-
-    await product.save({ validateBeforeSave:false });
-}
